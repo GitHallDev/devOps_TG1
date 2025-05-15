52072b06b5106e0df6fe8f7acb4d06389a9ec28d
catatraore <129023826+catatraore@users.noreply.github.com>
2025-05-15 16:58:54 +0000

    Update login.php
    juste pour voir si je peut directement modifier

--- modules/AuthModule/src/View/login.php
+++ modules/AuthModule/src/View/login.php
@@ -1,3 +1,4 @@
+// login
 <form action="/login" method="POST">
     <input type="email" name="email" id="" placeholder="Email" required>
     <input type="password" name="password" id="" placeholder = "Mot de passe" required>
